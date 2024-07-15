80660127d9ddb7c555dfe76e77ebdf5cb3e9db95
runpuchen <39805896+runpuchen@users.noreply.github.com>
2024-07-15 15:55:08 -0400

    Delete ReductionTreeCode directory

--- ReductionTreeCode/sqdist.m
+++
@@ -1,8 +0,0 @@
-function dist = sqdist(a,b)
-% a, b : [D, N] = size(a)
-% calculate the square distance between a, b
-
-aa = sum(a.^2,1); 
-bb = sum(b.^2,1); 
-ab = a'*b;
-dist = abs(repmat(aa',[1 size(bb,2)]) + repmat(bb,[size(aa,2) 1]) - 2*ab);
